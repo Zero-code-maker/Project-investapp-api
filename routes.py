976ac89f7cda6b508857c097b0ea5ecd4bd8d900
Rikerdaves Alencar <rikerdaves.alencar@hotmail.com>
2024-02-02 19:45:49 -0300

    Update

--- routes.py
+++ routes.py
@@ -8,15 +8,18 @@ router = APIRouter()
 db = get_database()
 operacoes_collection = db["acoes"]
 
-@router.post("/operacoes/cadastrar/")
+@router.post("/cadastrar")
 async def cadastrar_operacao(operacao: Operacao):
     operacao_dict = operacao.model_dump()
     operacao_dict["data"] = operacao_dict["data"].isoformat()
     operacao_id = operacoes_collection.insert_one(operacao_dict).inserted_id
     return {"mensagem": "Operação cadastrada com sucesso", "operacao_id": str(operacao_id)}
 
-@router.get("/operacoes/{operacao_id}", response_model=Operacao)
+@router.get("/operacao/{operacao_id}", response_model=Operacao)
 async def obter_operacao(operacao_id: str):
+    if not ObjectId.is_valid(operacao_id):
+        raise HTTPException(status_code=404, detail="Operação não encontrada")
+    
     operacao = operacoes_collection.find_one({"_id": ObjectId(operacao_id)})
     if operacao:
         operacao['_id'] = str(operacao['_id'])
@@ -24,7 +27,16 @@ async def obter_operacao(operacao_id: str):
     else:
         raise HTTPException(status_code=404, detail="Operação não encontrada")
 
-@router.get("/", response_model=List[Operacao])
+@router.get("/operacao/cod/{cod_acao}", response_model=List[Operacao])
+async def obter_operacao_cod(cod_acao: str):
+    operacoes = operacoes_collection.find({"cod_acao": cod_acao})
+    if operacoes:
+        operacoes = [dict(operacao, _id=str(operacao['_id'])) for operacao in operacoes]
+        return operacoes
+    else:
+        raise HTTPException(status_code=404, detail="Operações não encontradas para o código de ação fornecido")
+
+@router.get("/list", response_model=List[Operacao])
 async def listar_operacoes():
     operacoes = list(operacoes_collection.find())
     for operacao in operacoes:
